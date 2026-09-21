2c4afe6f71ab788b5591ecab6a4dc2e9fbc48454
Emily Horrana <128863617+emyHorrana@users.noreply.github.com>
2026-09-21 18:52:44 -0300

    Refatora estrutura de itens e implementa funcionalidades de inserção e similaridade

--- ProjetoC04.cpp
+++ ProjetoC04.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <list>
+
 using namespace std;
 
 // Pedro Gabriel da Silva 739
@@ -14,25 +16,115 @@ struct Item
     string dono;
     string propriedadeMagica;
     int id;
-    int raridade;      // 0 a 100
+    int raridade;
 };
 
+struct Aresta
+{
+    int origem;  
+    int destino;
+    int peso;
+};
+
+const int MAX_ITENS = 40000;
+Item itens[MAX_ITENS];
+int quantidadeItens = 0;
+
+list<Aresta> grafo[MAX_ITENS];
+
+int encontrarItem(int id)
+{
+    for(int i = 0; i < quantidadeItens; i++)
+    {
+        if(itens[i].id == id)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
 
 int inserirItem(string nomeItem, string nomeDono, string propriedadeMagica, int id, int raridade)
 {
-    cout << "Funcionalidade em construção" << endl;
-    return 0;
+    if(quantidadeItens >= MAX_ITENS) {
+        cout << "Inventário cheio!" << endl;
+        return 0;
+    }
+
+    if(encontrarItem(id) != -1) {
+        cout << "Erro: Já existe um item com o ID " << id << "!" << endl;
+        return 0;
+    }
+
+    itens[quantidadeItens].nome = nomeItem;
+    itens[quantidadeItens].dono = nomeDono;
+    itens[quantidadeItens].propriedadeMagica = propriedadeMagica;
+    itens[quantidadeItens].id = id;
+    itens[quantidadeItens].raridade = raridade;
+
+    quantidadeItens++;
+
+    cout << "Item inserido com sucesso!" << endl;
+    return 1;
 }
 
 int cadastrarSimilaridade(int idItem1, int idItem2, int similaridade)
 {
-    cout << "Funcionalidade em construção" << endl;
-    return 0;
+    int indice1 = encontrarItem(idItem1);
+    int indice2 = encontrarItem(idItem2);
+
+    if(indice1 == -1 || indice2 == -1) {
+        cout << "Erro: Um ou ambos os IDs não foram encontrados." << endl;
+        return 0;
+    }
+
+    // Cria a aresta usando as posições reais no vetor
+    grafo[indice1].push_back({indice1, indice2, similaridade});
+    grafo[indice2].push_back({indice2, indice1, similaridade});
+
+    cout << "Similaridade cadastrada com sucesso!" << endl;
+    return 1;
 }
 
-int buscarItensSimilares(int idItem, int valorMinimo)
+int buscarItensSimilares(int idItem, string nomeDono, int valorMinimo)
 {
-    cout << "Funcionalidade em construção" << endl;
+    int posicaoItem = encontrarItem(idItem);
+
+    if(posicaoItem == -1)
+    {
+        cout << "Item nao encontrado!" << endl;
+        return 0;
+    }
+
+    list<Aresta>::iterator it;
+    bool encontrou = false;
+
+    cout << "Itens similares encontrados:" << endl;
+
+    for(it = grafo[posicaoItem].begin(); it != grafo[posicaoItem].end(); it++)
+    {
+        if(it->peso > valorMinimo)
+        {
+            int posicaoDestino = it->destino;
+
+            if(itens[posicaoDestino].dono != nomeDono) 
+            {
+                cout << "ID: " << itens[posicaoDestino].id << endl;
+                cout << "Nome: " << itens[posicaoDestino].nome << endl;
+                cout << "Dono: " << itens[posicaoDestino].dono << endl;
+                cout << "Propriedade magica: " << itens[posicaoDestino].propriedadeMagica << endl;
+                cout << "Raridade: " << itens[posicaoDestino].raridade << endl;
+                cout << "Similaridade: " << it->peso << endl;
+                cout << "-----------------------------" << endl;
+                encontrou = true;
+            }
+        }
+    }
+
+    if (!encontrou) {
+        cout << "Nenhum item encontrado com esses critérios." << endl;
+    }
+
     return 0;
 }
 
@@ -42,11 +134,13 @@ int verificarItem(int idItem)
     return 0;
 }
 
+
 int listarItensAlfabeticamente()
 {
     cout << "Funcionalidade em construção" << endl;
     return 0;
 }
+
 
 int listarItensPorRaridade()
 {
@@ -54,17 +148,20 @@ int listarItensPorRaridade()
     return 0;
 }
 
+
 int contarPropriedadeMagica(string propriedadeMagica)
 {
     cout << "Funcionalidade em construção" << endl;
     return 0;
 }
 
+
 int removerItensMenosRaros(int raridadeMinima)
 {
     cout << "Funcionalidade em construção" << endl;
     return 0;
 }
+
 
 int main()
 {
@@ -76,16 +173,17 @@ int main()
     int similaridade;
     int valorMinimo;
     int raridadeMinima;
-
     string nomeItem;
     string nomeDono;
     string propriedadeMagica;
+
 
     while(opcao != 0)
     {
         cout << "=================================================" << endl;
         cout << "                    M E N U                      " << endl;
         cout << "=================================================" << endl;
+
         cout << "0. Sair                                          " << endl;
         cout << "1. Inserir item.                                 " << endl;
         cout << "2. Cadastrar similaridade entre itens.          " << endl;
@@ -95,14 +193,18 @@ int main()
         cout << "6. Listar itens (ordem decrescente de raridade). " << endl;
         cout << "7. Contar itens com mesma propriedade mágica.    " << endl;
         cout << "8. Remover itens menos raros.                    " << endl;
+
         cout << "-------------------------------------------------" << endl;
 
         cout << "Escolha uma opção: ";
         cin >> opcao;
 
+
         switch(opcao)
         {
+
             case 1:
+
                 cout << "Nome do item: ";
                 cin >> nomeItem;
 
@@ -114,14 +216,21 @@ int main()
 
                 cout << "ID: ";
                 cin >> id;
-
                 cout << "Raridade (0 a 100): ";
                 cin >> raridade;
 
-                inserirItem(nomeItem, nomeDono, propriedadeMagica, id, raridade);
+                inserirItem(
+                    nomeItem,
+                    nomeDono,
+                    propriedadeMagica,
+                    id,
+                    raridade
+                );
+
                 break;
 
             case 2:
+
                 cout << "ID do primeiro item: ";
                 cin >> idItem1;
 
@@ -131,59 +240,91 @@ int main()
                 cout << "Valor da similaridade: ";
                 cin >> similaridade;
 
-                cadastrarSimilaridade(idItem1, idItem2, similaridade);
+
+                cadastrarSimilaridade(
+                    idItem1,
+                    idItem2,
+                    similaridade
+                );
+
                 break;
 
             case 3:
+
                 cout << "ID do item: ";
                 cin >> id;
+
+                cout << "Nome do jogador: ";
+                cin >> nomeDono;
 
                 cout << "Valor minimo de similaridade: ";
                 cin >> valorMinimo;
 
-                buscarItensSimilares(id, valorMinimo);
+
+                buscarItensSimilares(
+                    id,
+                    nomeDono,
+                    valorMinimo
+                );
+
                 break;
 
             case 4:
+
                 cout << "ID do item: ";
                 cin >> id;
 
                 verificarItem(id);
+
                 break;
 
             case 5:
+
                 listarItensAlfabeticamente();
+
                 break;
 
             case 6:
+
                 listarItensPorRaridade();
+
                 break;
 
             case 7:
+
                 cout << "Propriedade magica: ";
                 cin >> propriedadeMagica;
 
                 contarPropriedadeMagica(propriedadeMagica);
+
                 break;
 
             case 8:
+
                 cout << "Raridade minima: ";
                 cin >> raridadeMinima;
 
                 removerItensMenosRaros(raridadeMinima);
+
                 break;
 
             case 0:
+
                 cout << "Saindo..." << endl;
+
                 break;
 
+
             default:
+
                 cout << "Entrada invalida!" << endl;
+
                 break;
         }
 
         cout << endl;
     }
+
 
     return 0;
 }
